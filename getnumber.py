d84ae5f1f4ee7c6a60f03e20a81a5dcc3da264cd
aaronshin43 <aaronshin.exe@gmail.com>
2025-02-23 23:05:39 -0500

    Detect apples in the game_screen.png

---
+++ getnumber.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("game_screen.png")
+#Loads the screenshot file "game_screen.png" into memory as a color image
+#3D NumPy array (height × width × 3 channels: Blue, Green, Red—BGR order)
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#Converts the color image (img) to grayscale (gray)
+#Grayscale simplifies the image to a 2D array (height × width)
+
+circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, dp=1, minDist=50,
+                           param1=50, param2=30, minRadius=17, maxRadius=30)
+
+if circles is not None:
+    circles = np.round(circles[0, :]).astype("int")
+    apples = 0
+    for (x, y, r) in circles:
+        #print(f"Apple at ({x}, {y})")
+        apples +=1
+    print(apples)
